e2799fac8f8660ccc0af921e4bed2b70110f8788
Harshit Srivastava <harshit.srivastava@mindruby.com>
2024-05-07 11:39:50 +0530

    make test fail

--- test/api_test.dart
+++ test/api_test.dart
@@ -10,7 +10,7 @@ void main(){
     bool value = false;
     var fetch = (await  Repo.getData()).isValid;
     if (fetch != null) {
-      value = true;
+      value = false;
     }
     expect(value, true);   
   });
